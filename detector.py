c8f633a64dc56d1021c9f6fe7fae3933b2f1b144
moatez amairi <35851660+M-AMAIRI@users.noreply.github.com>
2024-06-03 15:18:08 -0400

    update detector delete unused lin code

--- detector.py
+++ detector.py
@@ -8,6 +8,8 @@ THREAT_RULES = {
     "suspicious_processes": []  # Example rule: Suspicious process names
 }
 
+threats = []
+
 def update_suspicious_processes_from_file():
     with open("suspicious_processes.txt", 'r') as file:
         # Read lines from the file and strip any extra whitespace characters
@@ -20,39 +22,46 @@ def detect_threats():
     update_suspicious_processes_from_file()
     conn = sqlite3.connect('edr.db')
     c = conn.cursor()
-    threats = []
+    
 
     # Rule 1: Detect high CPU usage
     c.execute("SELECT * FROM processes WHERE cpu_percent > ?", (THREAT_RULES["cpu_percent"],))
     threats.extend(c.fetchall())
-    high_cpu_processes = c.fetchall()
-    insert_query = """
-        INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
-        VALUES (?,?,?,?,?);
-        """
-    c.executemany(insert_query, high_cpu_processes)
+    # high_cpu_processes = c.fetchall()
+    # insert_query = """
+    #     INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
+    #     VALUES (?,?,?,?,?);
+    #     """
+    # c.executemany(insert_query, high_cpu_processes)
     
     # Rule 2: Detect high memory usage
     c.execute("SELECT * FROM processes WHERE memory_percent > ?", (THREAT_RULES["memory_percent"],))
     threats.extend(c.fetchall())
-    suspicious_processes_memory_percent = c.fetchall()
-    insert_query = """
-        INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
-        VALUES (?,?,?,?,?);
-        """
-    c.executemany(insert_query, suspicious_processes_memory_percent)
+    # suspicious_processes_memory_percent = c.fetchall()
+    # insert_query = """
+    #     INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
+    #     VALUES (?,?,?,?,?);
+    #     """
+    # c.executemany(insert_query, suspicious_processes_memory_percent)
         
     # Rule 3: Detect suspicious process names
     for proc_name in THREAT_RULES["suspicious_processes"]:
         c.execute("SELECT * FROM processes WHERE name LIKE ?", (proc_name,))
         threats.extend(c.fetchall())
-        suspicious_processes_name = c.fetchall()
-        insert_query = """
+        # suspicious_processes_name = c.fetchall()
+        # insert_query = """
+        # INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
+        # VALUES (?,?,?,?,?);
+        # """
+        # c.executemany(insert_query, threats)
+
+
+    insert_query = """
         INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
         VALUES (?,?,?,?,?);
         """
-        c.executemany(insert_query, threats)
-
+    c.executemany(insert_query, threats)
+        
     conn.commit()
     conn.close()
     return threats
@@ -63,16 +72,16 @@ if __name__ == "__main__":
     
     
     
-    # Insert the high CPU processes into the threat table
-    insert_query = """
-    INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
-    VALUES (?,?,?,?,?);
-    """
-    c.executemany(insert_query, high_cpu_processes)
+    # # Insert the high CPU processes into the threat table
+    # insert_query = """
+    # INSERT INTO threats (pid, name, username, cpu_percent, memory_percent )
+    # VALUES (?,?,?,?,?);
+    # """
+    # c.executemany(insert_query, high_cpu_processes)
     
-    high_cpu_processes
-    suspicious_processes_name
-    suspicious_processes_memory_percent
+    # high_cpu_processes
+    # suspicious_processes_name
+    # suspicious_processes_memory_percent
 
     # Commit the changes and close the connection
     conn.commit()
